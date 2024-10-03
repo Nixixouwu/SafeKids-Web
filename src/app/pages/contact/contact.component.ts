4477dfa82383ef38edfacd4863910697b3b6b3cb
Nixixouwu <ni.aliaga@duocuc.cl>
2024-10-03 16:18:29 -0300

    Detalles Contacto

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HeaderComponent } from '../../components/header/header.component';
+import { CommonModule } from '@angular/common';
+import { NumbersOnlyDirective } from '../../validators/numbers-only.validator';
+
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ReactiveFormsModule, CommonModule, NumbersOnlyDirective],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  contactForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      institution: ['', Validators.required],
+      phone: ['', [Validators.pattern('^[0-9]*$')]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      // Here you can add the logic to send the form data
+    }
+  }
 }
